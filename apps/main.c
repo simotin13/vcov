1abbd991f4d3a6596b8bed174f4f88ebac19307e
Hiroyuki Miyazaki <miyazaki@mcommit.com>
2023-09-02 23:56:31 +0900

    delete main.c

--- apps/main.c
+++ apps/main.c
@@ -1,10 +1,19 @@
-#include <stdio.h>
+#include <iostream>
+
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-    if (argc < 2) {
+    if (argc < 3) {
         printf("Usage vcov <target_module>");
-        return -1;
+        return 1;
     }
-    open("/dev");
+
+    std::string cmd = "./create_dev_file " + argv[1]
+    int ret = system(cmd.c_str());
+    if (ret != 0) {
+        fprintf(stderr, "create dev file failed\n");
+        return 1;
+    }
+    return 0;
 }
