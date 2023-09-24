18f441f0250a2d9c731a9a5d4f760a5270d364ae
Hiroyuki Miyazaki <miyazaki@mcommit.com>
2023-09-25 00:20:59 +0900

    WIP mmapによる書き込みとreadでの読み出しを確認

--- apps/mmap.c
+++ apps/mmap.c
@@ -5,12 +5,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 
 #define DEVNAME	"/dev/vmm"
 int main(int argc, char **argv)
 {
+    char buf[128];
     char *address;
     int fd;
+    int ret;
     fd = open(DEVNAME, O_RDWR);
     if (fd<=0) {
         perror(DEVNAME);
@@ -24,7 +27,15 @@ int main(int argc, char **argv)
         return -1;
     }
     printf("address:[%p]\n", address);
-    printf("[%s]\n",address);
+    printf("[%s]\n", address);
+
+    ret = read(fd, buf, 32);
+    fprintf(stdout, "read result:[%d], buf:[%s]\n", ret, buf);
+
+    strcpy(address, "write test");
+
+    ret = read(fd, buf, 32);
+    fprintf(stdout, "read result:[%d], buf:[%s]\n", ret, buf);
 
     fprintf(stdout, "finish...\n");
     close(fd);
