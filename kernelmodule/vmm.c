38f55d55cba5e543e883a9becf0b30a27bb52c91
Hiroyuki Miyazaki <miyazaki@mcommit.com>
2023-09-27 13:51:15 +0900

    WIP

--- kernelmodule/vmm.c
+++ kernelmodule/vmm.c
@@ -4,6 +4,8 @@
 #include <linux/proc_fs.h>
 #include <asm/uaccess.h> // copy_from_user, copy_to_user
 #include <linux/mm.h>
+#include "cpu.h"
+#include "kernelmod.h"
 
 MODULE_LICENSE("Dual BSD/GPL");
 
@@ -26,11 +28,12 @@ static unsigned int vmm_major;
 // ============================================================================
 static int vmm_init(void);
 static void vmm_exit(void);
-static int vmm_open(struct inode *inode, struct file *fp);
-static int vmm_release(struct inode *inode, struct file *fp);
-static ssize_t vmm_read(struct file *fp, char __user *buf, size_t count, loff_t *f_pos);
-static ssize_t vmm_write(struct file *fp, const char __user *buf, size_t count, loff_t *f_pos);
-static int vmm_mmap(struct file *fp, struct vm_area_struct *vma);
+static int vmm_open(struct inode *inode, struct file *filep);
+static int vmm_release(struct inode *inode, struct file *filep);
+static ssize_t vmm_read(struct file *filep, char __user *buf, size_t count, loff_t *f_pos);
+static ssize_t vmm_write(struct file *filep, const char __user *buf, size_t count, loff_t *f_pos);
+static int vmm_mmap(struct file *filep, struct vm_area_struct *vma);
+static long vmm_ioctl(struct file *filep, unsigned int cmd, unsigned long arg);
 
 static void vm_open(struct vm_area_struct *vma);
 static void vm_close(struct vm_area_struct *vma);
@@ -42,6 +45,7 @@ struct file_operations devone_fops = {
     .read = vmm_read,
     .write = vmm_write,
     .mmap = vmm_mmap,
+    .unlocked_ioctl = vmm_ioctl,
 };
 
 static struct vm_operations_struct vm_ops =
@@ -92,7 +96,7 @@ static void vmm_exit(void)
     printk(KERN_DEBUG "vmm driver removed.\n");
 }
 
-static int vmm_open(struct inode *inode, struct file *fp) 
+static int vmm_open(struct inode *inode, struct file *filep)
 {
     VMM_MMAP_INFO *vmm_mmap_info;
     printk(KERN_DEBUG "vmm_open called...\n");
@@ -101,18 +105,18 @@ static int vmm_open(struct inode *inode, struct file *fp)
     vmm_mmap_info->data = (char *)get_zeroed_page(GFP_KERNEL);
 
     memcpy(vmm_mmap_info->data, "hello from kernel this is file: ", 32);
-    fp->private_data = vmm_mmap_info;
+    filep->private_data = vmm_mmap_info;
 
     return 0;
 }
 
-static int vmm_release(struct inode *inode, struct file *fp)
+static int vmm_release(struct inode *inode, struct file *filep)
 {
     printk(KERN_DEBUG "vmm_close call...\n");
     return 0;
 }
 
-ssize_t vmm_read(struct file *fp, char __user *buf, size_t count, loff_t *f_pos)
+ssize_t vmm_read(struct file *filep, char __user *buf, size_t count, loff_t *f_pos)
 {
     int i;
     unsigned char val = 0xff;
@@ -130,19 +134,19 @@ ssize_t vmm_read(struct file *fp, char __user *buf, size_t count, loff_t *f_pos)
 
     return retval;
 }
-static ssize_t vmm_write(struct file *fp, const char __user *buf, size_t count, loff_t *f_pos)
+static ssize_t vmm_write(struct file *filep, const char __user *buf, size_t count, loff_t *f_pos)
 {
     printk(KERN_DEBUG "vmm_write called...\n");
     return 0;
 }
 
 
-static int vmm_mmap(struct file *fp, struct vm_area_struct *vma)
+static int vmm_mmap(struct file *filep, struct vm_area_struct *vma)
 {
     printk(KERN_DEBUG "vmm_mmap called...\n");
     vma->vm_ops = &vm_ops;
     vma->vm_flags |= VM_DONTEXPAND | VM_DONTDUMP;
-    vma->vm_private_data = fp->private_data;
+    vma->vm_private_data = filep->private_data;
     vm_open(vma);
     return 0;
 }
@@ -185,6 +189,83 @@ static vm_fault_t vm_fault(struct vm_fault *vmf)
     get_page(page);
     vmf->page = page;
     return 0;
+}
+
+static long vmm_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
+{
+    static VmmCtrl vmmCtrl;
+    int ret;
+    unsigned long long val = 0;
+    unsigned int b32Val;
+
+    printk(KERN_DEBUG "vmm_ioctl In...");
+
+    memset(&vmmCtrl, 0, sizeof(VmmCtrl));
+    ret = raw_copy_from_user(&vmmCtrl, (void __user *)arg, sizeof(VmmCtrl));
+    if (ret < 0) {
+        return -EFAULT;
+    }
+
+    printk(KERN_DEBUG "cmd:[0x%02X], addr:[0x%llX] val:[0x%llX]\n", cmd, vmmCtrl.addr, vmmCtrl.val);
+
+    switch(cmd) {
+    case VMM_READ_CR0:
+        val = _read_cr0();
+        printk(KERN_DEBUG "_read_cr0 val:[0x%llX]\n", val);
+        vmmCtrl.val = val;
+        ret = raw_copy_to_user((void __user *)arg, &vmmCtrl, sizeof(VmmCtrl));
+        if (ret != 0) {
+            ret = -EFAULT;
+        }
+        break;
+    case VMM_READ_CR4:
+        val = _read_cr4();
+        vmmCtrl.val = val;
+        ret = raw_copy_to_user((void __user *)arg, &vmmCtrl, sizeof(VmmCtrl));
+        if (ret != 0) {
+            ret = -EFAULT;
+        }
+        break;
+    case VMM_WRITE_CR4:
+        b32Val = vmmCtrl.val;
+        printk(KERN_DEBUG "_write_cr4 b32Val:[0x%X]\n", b32Val);
+        _write_cr4(b32Val);
+        ret = 0;
+        break;
+    case VMM_READ_MSR:
+        val = _read_msr(vmmCtrl.addr);
+        vmmCtrl.val = val;
+        ret = raw_copy_to_user((void __user *)arg, &vmmCtrl, sizeof(VmmCtrl));
+        if (ret != 0) {
+            ret = -EFAULT;
+        }
+        break;
+    case VMM_WRITE_MSR:
+        val = _write_msr(vmmCtrl.addr, vmmCtrl.val);
+        vmmCtrl.val = val;
+        ret = raw_copy_to_user((void __user *)arg, &vmmCtrl, sizeof(VmmCtrl));
+        if (ret != 0) {
+            ret = -EFAULT;
+        }
+        break;
+    case VMM_VMXOFF:
+        val = _vmxoff();
+        ret = raw_copy_to_user((void __user *)arg, &vmmCtrl, sizeof(VmmCtrl));
+        if (ret != 0) {
+            ret = -EFAULT;
+        }
+        break;
+    case VMM_VMXON:
+        val = _vmxon();
+        vmmCtrl.val = val;
+        ret = raw_copy_to_user((void __user *)arg, &vmmCtrl, sizeof(VmmCtrl));
+        break;
+    default:
+        printk(KERN_DEBUG "Unknown cmd:[0x%X] called.\n", cmd);
+        break;
+    }
+
+    return ret;
 }
 
 module_init(vmm_init);
