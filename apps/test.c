9f4415bea90a4f58704436b96ff8855e0ed270ee
Hiroyuki Miyazaki <miyazaki@mcommit.com>
2023-10-19 00:44:46 +0900

    WIP

--- apps/test.c
+++ apps/test.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include "cpu.h"
-
+#include "../kernelmodule/kernelmod.h"
 int main(int argc, char **argv)
 {
     char buf[16];
     memset(buf, 0, 16);
     _cpuid_id(buf);
     printf("%s\n", buf);
-    int vmx = _is_support_vmx();
-    printf("%d\n", vmx);
 
-    int cr4 = _enable_vmxe();
-    printf("cr4:%x\n", cr4);
     return 0;
 }
