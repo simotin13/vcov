2249bc79fce339139044ea7cac3dec77f77ce76b
Hiroyuki Miyazaki <miyazaki@mcommit.com>
2023-10-21 16:24:04 +0900

    WIP

--- setup_kernel_module.sh
+++ setup_kernel_module.sh
@@ -15,11 +15,12 @@ if [ $? -ne 0 ]; then
 fi
 
 MAJOR_NUM=$(cat /proc/devices | grep vmm | awk '{print $1}')
+echo "vmm driver major number ${MAJOR_NUM}"
 if [ ${MAJOR_NUM} = "" ]; then
   echo "vmm not found"
   exit 1
 fi
 
 cd ..
-sudo mknod -m 666 vmm c ${MAJOR_NUM} 2
-
+sudo rm -f /dev/vmm
+sudo mknod -m 666 /dev/vmm c ${MAJOR_NUM} 1
